c29f6708a3c690a01edec8c7a1a6dde6f1dc0a5f
Atharvnasare <nasareatharv2003@gmail.com>
2025-06-05 18:08:22 +0530

    new commit 2

--- ten.c
+++ ten.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
 
+/* how to read and write a file ( create a new file using write mode)*/
 int main (){
-    printf("Hello World");
-
-    printf("My name is devil");
+    FILE *fptr;
+    fptr = fopen("NewText.txt","w");
+    if(fptr == NULL){
+        printf("file doesn't exist \n");
+    }
+    else{
+        fclose(fptr);
+    }
+    
+    
+    return 0;
     
 }
