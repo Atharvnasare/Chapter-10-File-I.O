4e6759ae2e34d3fd7e71dbc7d0f5a3497cc2f9db
Atharvnasare <nasareatharv2003@gmail.com>
2025-06-05 19:11:39 +0530

    new commit 3

--- ten.c
+++ ten.c
@@ -1,17 +1,44 @@
 #include<stdio.h>
 
-/* how to read and write a file ( create a new file using write mode)*/
-int main (){
+/* how to read and write a file ( create a new file using write mode )*/
+// int main (){
+//     FILE *fptr;
+//     fptr = fopen("NewText.txt","w");
+//     if(fptr == NULL){
+//         printf("file doesn't exist \n");
+//     }
+//     else{
+//         fclose(fptr);
+//     }
+//     return 0;
+    
+// }
+
+int main(){
     FILE *fptr;
-    fptr = fopen("NewText.txt","w");
-    if(fptr == NULL){
-        printf("file doesn't exist \n");
-    }
-    else{
-        fclose(fptr);
-    }
-    
-    
+    fptr = fopen("Text.txt","r");
+
+    // char ch;
+    // fscanf(fptr , "%c" , &ch);
+    // printf("character = %c \n" , ch);
+    // fscanf(fptr , "%c" , &ch);
+    // printf("character = %c \n" , ch);
+    // fscanf(fptr , "%c" , &ch);
+    // printf("character = %c \n" , ch);
+    // fscanf(fptr , "%c" , &ch);
+    // printf("character = %c \n" , ch);
+    // fscanf(fptr , "%c" , &ch);
+    // printf("character = %c \n" , ch);
+
+    int ch;
+    fscanf(fptr , "%d" , &ch);
+    printf("character = %d \n" , ch);
+    fscanf(fptr , "%d" , &ch);
+    printf("character = %d \n" , ch);
+    fscanf(fptr , "%d" , &ch);
+    printf("character = %d \n" , ch);
+
+
+    fclose(fptr);
     return 0;
-    
 }
