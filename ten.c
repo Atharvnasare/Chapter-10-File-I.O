f1d129dc3b6ec9ab7cbc4262f59fdcc92238be09
Atharvnasare <nasareatharv2003@gmail.com>
2025-06-06 17:44:36 +0530

    new

--- ten.c
+++ ten.c
@@ -152,20 +152,20 @@
 /*2 number - a and b , are written in a file . 
 write a program to replace them with sum;*/
 
-int main(){
-    FILE *fptr;
-    fptr = fopen("Sum.txt" , "r");
+// int main(){
+//     FILE *fptr;
+//     fptr = fopen("Sum.txt" , "r");
 
-    int a;
-    fscanf(fptr , "%d" , &a);
-    int b;
-    fscanf(fptr , "%d" , &b);
+//     int a;
+//     fscanf(fptr , "%d" , &a);
+//     int b;
+//     fscanf(fptr , "%d" , &b);
 
-    fclose(fptr);
+//     fclose(fptr);
 
-    fptr = fopen("Sum.txt" , "w");
-    fprintf(fptr , "%d" , a+b);
-    fclose(fptr);
-    return 0;
+//     fptr = fopen("Sum.txt" , "w");
+//     fprintf(fptr , "%d" , a+b);
+//     fclose(fptr);
+//     return 0;
 
-}
+// }
